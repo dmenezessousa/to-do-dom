b5e5df30bf6e7837f1c6705462112ad274c56718
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-07-26 08:50:05 -0400

    added some notes

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
+//get button element 
 const button = $('#save');
 
-
+//on click function to get information from input and send it to a new empty DIV
 button.on('click',(e) =>{
-    const task = $('#task').val();
-    const desc = $('#desc').val();
-    const priority = $('input:radio[name=priority]:checked').val();
-    console.log(priority);
-    e.preventDefault();
+    const task = $('#task').val();//get values from task
+    const desc = $('#desc').val();//get values from description
+    const priority = $('input:radio[name=priority]:checked').val();//get which radio input was clicked
+    e.preventDefault();//prevent submit button to send out the page information.
+    //create a new DIV and log out the values from Radio input,task and description.
     const newElement = "<div class='todo " + priority +" '><p>"+ task +"</p><p>"+ desc +"</p><p>"+ priority +"</p><button class='delTodo'>X</button></div>";
+    //add the new div inside my empty div in html.
     $('#task-list').append(newElement);
+    //restore task input to empty
     $('#task').val('');
+    //restore description input to empty
     $('#desc').val('');
-    console.log(newElement);
-    console.log('clickei');
 });
 
-
+//function to remove one of the todo itens from my DIV.
 $('#task-list').on('click', '.todo .delTodo', function() {
     $(this).parent().remove();
-    console.log( 'foi');
 });
 
+//function for the remove all button
 $('#remove-all').on('click', function() {
     $('#task-list').html('');
-    console.log( 'foi');
 });
-
-
-
-
-
-
